4d7a0ec001137d0836d63db32c541f40653a30dc
SleepiCaffeine <depressedmachiato@gmail.com>
2025-01-02 16:11:07 +0200

    - Moved functions around - Adjusted REGEX to capture functions properly

--- RegexFileParser.hpp
+++ RegexFileParser.hpp
@@ -3,14 +3,15 @@
 #include <string>
 #include <regex>
 
-// Should be good for checking functions
-// ([A-Za-z_\(\*&][\w:< >,&\*\(\)]*\s+)+([A-Za-z_]\w*)\s*\(((\s*[A-Za-z_\(\*&][\w:<>,&\*\(\)]*\s+)+([A-Za-z_]\w*)\s*,?)*\s*\)\s*([A-Za-z_]\w*\s*)*;
-// Full description: (?<returnType>[A-Za-z_\(\*&][\w:< >,&\*\(\)]*\s+)+(?<funcName>[A-Za-z_]\w*)\s*\((?<PARAMETER>(?<paramType>\s*[A-Za-z_\(\*&][\w:<>,&\*\(\)]*\s+)+(?<paramName>[A-Za-z_]\w*)\s*,?)*\s*\)\s*(?<Qualifiers>[A-Za-z_]\w*\s*)*; 
-
 // Returns a vector of all regex matches within the provided string.
 // `match` must be a raw string literal, with an R"" prefix.
 // Example: R"(w+\s)"
 std::vector< std::smatch > getRegexMatches(const std::string& content, const char* match);
 
+std::ifstream openReadFile(const char* fileName);
 
+std::string extractFileContent(const std::ifstream& file_stream);
 
+std::ofstream openWriteFile(const char* fileName);
+
+std::string getline(std::ifstream& file);
--- slpUtility.hpp
+++ slpUtility.hpp
@@ -1,15 +1,8 @@
 #pragma once
 #include <string>
-#include <fstream>
 
 
 bool containsInOrder(const std::string& str, const std::string& chars);
-
-std::ifstream openReadFile(const char* fileName);
-
-std::string extractFileContent(const std::ifstream& file_stream);
-
-std::ofstream openWriteFile(const char* fileName);
 
 // trim from end
 std::string rtrim(const std::string& s);
