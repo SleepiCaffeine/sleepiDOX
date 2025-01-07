2ac7bb01669414330cde6229983c78e0776ad13b
SleepiCaffeine <depressedmachiato@gmail.com>
2025-01-07 16:24:10 +0200

    -Moved code around - Changed #define to using and constexpr - Removed already defined function definitions

--- sleepiDOX.hpp
+++ sleepiDOX.hpp
@@ -2,35 +2,32 @@
 #include <string>
 #include <fstream>
 #include <array>
+#include <unordered_map>
+#include <iostream>
 
+constexpr size_t ENTRY_FUNCTION_NAME = 0;
+constexpr size_t ENTRY_COMMENT = 1;
+constexpr size_t ENTRY_RETURNS = 2;
+constexpr size_t ENTRY_PARAMS = 3;
+constexpr size_t ENTRY_FUNCTION_DEFINTION = 4;
+constexpr size_t ENTRY_FUNCTION_CLASS = 5;
 
+using DOXEntry     = std::array<std::string, 5>;
+using DOXContainer = std::unordered_map< std::string, std::vector <DOXEntry> >;
 
-#define ENTRY_FUNCTION_NAME 0
-#define ENTRY_COMMENT 1
-#define ENTRY_RETURNS 2
-#define ENTRY_PARAMS 3
-#define ENTRY_FUNCTION_DEFINTION 4
-#define ENTRY_FUNCTION_CLASS 5
+/* @sleepiDOX Extracts the command-line arguments passed in. The required flags are:
+- -d : output file destrination/directory. Must exist.
+- -s : source file that will be parsed.
+*/
+// @sleepiPARAM args : length of `argv`
+// @sleepiPARAM argv : arguments passed into `int main()`
+// @sleepiPARAM rfile : string that will contain input file.
+// @sleepiPARAM wfile : string that will contain output file.
+// @sleepiRETURNS Returns true if all operations were successful, otherwise false.
+bool extractArguments(const int& args, const char* argv[], std::string& rfile, std::string& wfile);
 
-using DOXEntry = std::array<std::string, 5>;
-
-
-std::ifstream openReadFile(const char* fileName);
-std::ofstream openWriteFile(const char* fileName);
-	/*
-	*   How this will work - sleepiDOX will scan each line of every file provided.
-	*	It will look for "keys", the main one will be "@sleepiDOX"
-	*	Upon finding said key, it will start saving each line it reads until it encounters a function declaration.
-	* 
-	*	The method will work as such:
-	*		- Scan every line
-	*		- If the beginning of the line contains @sleepiDOX:
-	*			- Create a basic template of what the entry will look like: "Returns", "Functionality", "Parameters"
-	*			- Add The entire comment* into "Functionality"
-	*			_ *: there are reserved keys like @Returns, @Param, @Warning, @TODO, @Heading# which will have their own separate entries, and will overrdie defaults
-	*			- When sleepiDOX reaches the function declaration - it will add the "Returns" and "Parameters" properties.
-	
-			- If the beginning of the line contains @sleepiCustomDOX
-				- It will serialize the entire comment looking for "@_____" style keys, and will populate the entry as provided
-				- It will not add any additional properties to the entry.
-	*/
+// @sleepiDOX Fills the provided file output stream with preformatted documentation text.
+// @sleepiPARAM output_file : output file stream where this function will write to. Does not perform any validation.
+// @sleepiPARAM entries: hashmap of entries to document.
+// @sleepiPARAM title *(optional)*: Text at the top of the page, at header level 1
+void generateDocFile(std::ofstream& output_file, const DOXContainer& entries, const std::string_view& title = "");
