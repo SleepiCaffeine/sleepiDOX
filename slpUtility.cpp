12156c0fbb9a160b38d91868f04d15016ea003ae
Vakaris Michejenko <depressedmachiato@gmail.com>
2025-01-10 19:08:06 +0200

    - Changed almost all C-style strings into either std::string or std::string_view - Changed some constants to have the Sleepi:: qualifier

--- slpUtility.cpp
+++ slpUtility.cpp
@@ -7,7 +7,7 @@
 
 // Checks whether `str` contains every character in `chars` in the order provided.
 // Returns true if so.
-bool containsInOrder(const std::string& str, const std::string& chars) {
+bool containsInOrder(const std::string& str, const std::string_view& chars) {
     size_t last_pos = 0;
     return std::none_of(chars.cbegin(), chars.cend(), [&str, &last_pos](char ch) {
         last_pos = str.find(ch, last_pos);
@@ -17,7 +17,7 @@ bool containsInOrder(const std::string& str, const std::string& chars) {
 
 
 // trim from end
-std::string rtrim(const std::string& s) {
+std::string rtrim(const std::string_view& s) {
     std::string copy{ s };
     const size_t pos = copy.find_last_not_of(" w\t\f\v\r\n") + 1;
     if (pos >= copy.size())
@@ -25,12 +25,12 @@ std::string rtrim(const std::string& s) {
     return copy.erase(pos);
 }
 // trim from start
-std::string ltrim(const std::string& s) {
+std::string ltrim(const std::string_view& s) {
     std::string copy{ s };
     return copy.erase(0, copy.find_first_not_of(" w\t\f\v\r\n"));
 }
 
-std::string rltrim(const std::string& str) {
+std::string rltrim(const std::string_view& str) {
     return ltrim(rtrim(str));
 }
 
--- slpUtility.hpp
+++ slpUtility.hpp
@@ -2,14 +2,14 @@
 #include <string>
 
 
-bool containsInOrder(const std::string& str, const std::string& chars);
+bool containsInOrder(const std::string& str, const std::string_view& chars);
 
 // trim from end
-std::string rtrim(const std::string& s);
+std::string rtrim(const std::string_view& s);
 // trim from start
-std::string ltrim(const std::string& s);
+std::string ltrim(const std::string_view& s);
 // trim from both sides
-std::string rltrim(const std::string& str);
+std::string rltrim(const std::string_view& str);
 
 // Removes given token from string. Additionally removes multiline-comment end token.
 // Returns an edited copy of the string
