4bea7fd234714a9c24986a2c29c383ff3c7bfbd2
Vakaris Michejenko <depressedmachiato@gmail.com>
2025-01-10 19:10:01 +0200

    - Added Sleepi namespace - Added Sleepi::DOXContext struct to store information about the comand line argument - Changed `extractArguments` to take a vector, and return a context - Added `validateContext` - Added Sleepi::ErrorBits namespace and error constants
    - First version of parsing multiple files at once. Still WIP

--- sleepiDOX.cpp
+++ sleepiDOX.cpp
@@ -1,49 +1,97 @@
 #include "sleepiDOX.hpp"
-#include <string>
-#include <iostream>
-#include <unordered_map>
+#include <filesystem>
+#include <stdexcept>
 
-bool extractArguments(const int& args, const char* argv[], std::string& rfile, std::string& wfile) {
+Sleepi::DOXContext extractArguments(const std::vector<std::string>& argv, const bool strict) {
     // Possible future flags:
-    // -fs  :   Parses entire folder for files
-    // -dd  :   Destination directory (relative to executable)
     // -xp  :   Would attempt to parse multiple files looking for things like #define's, "using"s, and such to find what things are defined as. [Most likely not]
-    
-    
-    if (args <= 4) {
-        std::cout << "You need to supply the file to parse with the flag \"-s\", and destination file with \"-d\"" << std::endl;
-        return false;
+
+    namespace sErr = Sleepi::ErrorBits;
+    Sleepi::DOXContext context =  { {}, "", 0};
+
+    // Looping up to .size() - 1, because every flag has to have a second string afterward
+    for (size_t index = 0; index < argv.size() - 1; ++index) {
+        const std::string_view argument = argv.at(index);
+
+        // Flag that dictates the source file
+        if (argument == "-s") {
+            context.sourceDirs.push_back(argv.at(index + 1));
+        }
+        // Flag that dictates the destination file
+        else if (argument == "-d") {
+            context.outputFileDir = argv.at(index + 1);
+        }
+        // Flag that dictates the directory where the source files are kept
+        else if (argument == "-fs") {
+            namespace fs = std::filesystem;
+
+            const std::string_view directory = argv.at(index + 1);
+            const fs::path inputDirPath = directory;
+
+            // Make sure the file actually exists, and if not, set an error flag
+            if (!fs::exists(inputDirPath)) {
+                context.errorFlags |= sErr::InputDirDoesntExist;
+                return context;
+            }
+
+            // Loop through every file in the directory
+            for (const auto& directory_entry : fs::directory_iterator(inputDirPath)) {
+                
+                // Ignore every file that isn't a "regular" aka text file
+                if (!directory_entry.is_regular_file())
+                    continue;
+                // check if the file is a header file
+                const fs::path current_path = directory_entry.path();
+                if (current_path.extension() == ".hpp" || current_path.extension() == ".h")
+                    context.sourceDirs.push_back(current_path.string());
+            }
+
+            // At this point, all the files have been found, but there's a chance there were no headers
+            if (context.sourceDirs.empty())
+                context.errorFlags |= sErr::NoInputFilesFound;
+
+        }
     }
 
-    bool have_rfile = false, have_wfile = false;
-    if (argv[1][0] != '-' || argv[3][0] != '-') {
-        std::cout << "You need to supply the file to parse with the flag \"-s\", and destination file with \"-d\"" << std::endl;
-        std::cout << "Found: \"" << argv[1] << "\", \"" << argv[3] << "\"" << std::endl;
+    if (context.outputFileDir.empty())
+        context.errorFlags |= sErr::NoOutputFileSpecified;
+
+    // If the -fs flag was not specified, and the input is still empty
+    // This means that no input was provided at all.
+
+    // This could've been done with some sort of bool like:
+    // `bool inputProvided = false;` but eh, doesn't really change much
+    if ((context.errorFlags & sErr::NoInputFilesFound) == 0
+        && context.sourceDirs.empty()) {
+        context.errorFlags |= sErr::NoInputSpecified;
     }
 
+    if (strict)
+        validateContext(context);
 
-    if (argv[1][1] == 's' || argv[3][1] == 's') {
-        rfile = argv[2 + ((argv[1][1] == 's') ? 0 : 2)];
-    }
-    else {
-        std::cout << "No \"-s\" flag detected! Please reference the documentation for required flags.\n";
-        std::cout << "Found: \"" << argv[1] << "\", \"" << argv[3] << "\"" << std::endl;
-        return false;
-    }
-    if (argv[1][1] == 'd' || argv[3][1] == 'd') {
-        wfile = argv[2 + ((argv[1][1] == 'd') ? 0 : 2)];
-    }
-    else {
-        std::cout << "No \"-d\" flag detected! Please reference the documentation for required flags." << std::endl;
-        std::cout << "Found: \"" << argv[1] << "\", \"" << argv[3] << "\"" << std::endl;
-        return false;
-    }
-
-
-    return true;
+    return context;
 }
 
-void generateDocFile(std::ofstream& output_file, const DOXContainer& entries, const std::string_view& title) {
+void validateContext(const Sleepi::DOXContext& context) {
+    namespace Err = Sleepi::ErrorBits;
+    const size_t& flags = context.errorFlags;
+    
+
+    if (flags & Err::InputDirDoesntExist)
+        throw std::system_error( std::error_code(), "The provided input directory does not exist.");
+    if (flags & Err::NoInputFilesFound)
+        throw std::system_error(std::error_code(), "The program was unable to find the file(s) specified.");
+    if (flags & Err::NoInputSpecified)
+        throw std::logic_error("No input flag provided. Use the flags -s or -fs to provide a file to parse.");
+    if (flags & Err::NoOutputFileSpecified)
+        throw std::logic_error("No output flag provided. Use the flag -d to provide the destination.");
+
+}
+
+
+void generateDocFile(std::ofstream& output_file, const Sleepi::DOXContainer& entries, const std::string_view& title) {
+
+    using namespace Sleepi;
 
     // Markdown really likes double newlines
     constexpr char MD_NL[] = "\n\n";
