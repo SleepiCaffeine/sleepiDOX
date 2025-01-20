13ab06c71c408cbcc406dc625b6c3b87d88cfc3c
Vakaris Michejenko <depressedmachiato@gmail.com>
2025-01-21 00:20:14 +0200

    very minor changes

--- sleepiDOX.cpp
+++ sleepiDOX.cpp
@@ -3,175 +3,188 @@
 #include <stdexcept>
 #include <string>
 #include <algorithm>
+#include "RegexFileParser.hpp"
 
 Sleepi::DOXContext extractArguments(const std::vector<std::string>& argv, const bool strict) {
-    // Possible future flags:
-    // -xp  :   Would attempt to parse multiple files looking for things like #define's, "using"s, and such to find what things are defined as. [Most likely not]
+  // Possible future flags:
+  // -xp  :   Would attempt to parse multiple files looking for things like #define's, "using"s, and such to find what things are defined as. [Most likely not]
 
-    namespace sErr = Sleepi::ErrorBits;
-    Sleepi::DOXContext context =  { {}, "", 0};
+  namespace sErr = Sleepi::ErrorBits;
+  Sleepi::DOXContext context =  { {}, "", 0};
 
-    // Looping up to .size() - 1, because every flag has to have a second string afterward
-    for (size_t index = 1; index < argv.size() - 1; index += 2) {
-        const std::string_view argument = argv.at(index);
+  // Looping up to .size() - 1, because every flag has to have a second string afterward
+  for (size_t index = 1; index < argv.size() - 1; index += 2) {
+    const std::string_view argument = argv.at(index);
 
-        // Flag that dictates the source file
-        if (argument == "-s") {
-            context.sourceDirs.push_back(argv.at(index + 1));
-        }
-        // Flag that dictates the destination file
-        else if (argument == "-d") {
-            context.outputFileDir = argv.at(index + 1);
-        }
-        // Flag that dictates the directory where the source files are kept
-        else if (argument == "-fs") {
-            namespace fs = std::filesystem;
-
-            std::string directory = argv.at(index + 1);
-            if (directory.at(0) == '-')
-                directory = argv.at(0);
-
-            fs::path inputDirPath = directory;
-            if (inputDirPath.has_filename())
-                inputDirPath = inputDirPath.parent_path();
-
-
-            // Make sure the directory actually exists, and if not, set an error flag
-            if (!fs::exists(inputDirPath)) {
-                context.errorFlags |= sErr::InputDirDoesntExist;
-                return context;
-            }
-
-            // Loop through every file in the directory
-            for (const auto& directory_entry : fs::directory_iterator(inputDirPath)) {
-                
-                // Ignore every file that isn't a "regular" aka text file
-                if (!directory_entry.is_regular_file())
-                    continue;
-                // check if the file is a header file
-                const fs::path current_path = directory_entry.path();
-                if (current_path.extension() == ".hpp" || current_path.extension() == ".h")
-                    context.sourceDirs.push_back(current_path.string());
-            }
-
-            // At this point, all the files have been found, but there's a chance there were no headers
-            if (context.sourceDirs.empty())
-                context.errorFlags |= sErr::NoInputFilesFound;
-
-        }
+    // Flag that dictates the source file
+    if (argument == "-s") {
+      context.sourceDirs.push_back(argv.at(index + 1));
     }
+    // Flag that dictates the destination file
+    else if (argument == "-d") {
+      context.outputFileDir = argv.at(index + 1);
+    }
+    // Flag that dictates the directory where the source files are kept
+    else if (argument == "-fs") {
+      namespace fs = std::filesystem;
+
+      std::string directory = argv.at(index + 1);
+      if (directory.at(0) == '-')
+        directory = argv.at(0);
+
+      fs::path inputDirPath = directory;
+      if (inputDirPath.has_filename())
+        inputDirPath = inputDirPath.parent_path();
+
+
+      // Make sure the directory actually exists, and if not, set an error flag
+      if (!fs::exists(inputDirPath)) {
+        context.errorFlags |= sErr::InputDirDoesntExist;
+        return context;
+      }
+
+      // Loop through every file in the directory
+      for (const auto& directory_entry : fs::directory_iterator(inputDirPath)) {
+        
+        // Ignore every file that isn't a "regular" aka text file
+        if (!directory_entry.is_regular_file())
+          continue;
+        // check if the file is a header file
+        const fs::path current_path = directory_entry.path();
+        if (current_path.extension() == ".hpp" || current_path.extension() == ".h")
+          context.sourceDirs.push_back(current_path.string());
+      }
+
+      // At this point, all the files have been found, but there's a chance there were no headers
+      if (context.sourceDirs.empty())
+        context.errorFlags |= sErr::NoInputFilesFound;
+
+    }
+  }
 
    // Commented because new default behavior will now extract each file separately
    // Will remove after a stable version
    /* if (context.outputFileDir.empty())
-        context.errorFlags |= sErr::NoOutputFileSpecified;*/
+    context.errorFlags |= sErr::NoOutputFileSpecified;*/
 
-    // If the -fs flag was not specified, and the input is still empty
-    // This means that no input was provided at all.
+  // If the -fs flag was not specified, and the input is still empty
+  // This means that no input was provided at all.
 
-    // This could've been done with some sort of bool like:
-    // `bool inputProvided = false;` but eh, doesn't really change much
-    if ((context.errorFlags & sErr::NoInputFilesFound) == 0
-        && context.sourceDirs.empty()) {
-        context.errorFlags |= sErr::NoInputSpecified;
-    }
+  // This could've been done with some sort of bool like:
+  // `bool inputProvided = false;` but eh, doesn't really change much
+  if ((context.errorFlags & sErr::NoInputFilesFound) == 0
+    && context.sourceDirs.empty()) {
+    context.errorFlags |= sErr::NoInputSpecified;
+  }
 
-    if (strict)
-        validateContext(context);
+  if (strict)
+    validateContext(context);
 
-    return context;
+  return context;
 }
 
 void validateContext(const Sleepi::DOXContext& context) {
-    namespace Err = Sleepi::ErrorBits;
-    const size_t& flags = context.errorFlags;
-    
+  namespace Err = Sleepi::ErrorBits;
+  const size_t& flags = context.errorFlags;
+  
 
-    if (flags & Err::InputDirDoesntExist)
-        throw std::system_error( std::error_code(), "The provided input directory does not exist.");
-    if (flags & Err::NoInputFilesFound)
-        throw std::system_error(std::error_code(), "The program was unable to find the file(s) specified.");
-    if (flags & Err::NoInputSpecified)
-        throw std::logic_error("No input flag provided. Use the flags -s or -fs to provide a file to parse.");
-    if (flags & Err::NoOutputFileSpecified)
-        throw std::logic_error("No output flag provided. Use the flag -d to provide the destination.");
+  if (flags & Err::InputDirDoesntExist)
+    throw std::system_error( std::error_code(), "The provided input directory does not exist.");
+  if (flags & Err::NoInputFilesFound)
+    throw std::system_error(std::error_code(), "The program was unable to find the file(s) specified.");
+  if (flags & Err::NoInputSpecified)
+    throw std::logic_error("No input flag provided. Use the flags -s or -fs to provide a file to parse.");
+  if (flags & Err::NoOutputFileSpecified)
+    throw std::logic_error("No output flag provided. Use the flag -d to provide the destination.");
 
 }
 
-std::string replaceString(std::string& source, const std::string_view& search,
-    const std::string_view& replace ) {
-    std::string::size_type pos = 0;
-    while ( (pos = source.find(search, pos)) != std::string::npos) {
-        source.replace(pos, search.length(), replace);
-        pos += replace.size();
-    }
-    return source;
+std::string replaceString(std::string& source, const std::string_view& search, const std::string_view& replace ) {
+  std::string::size_type pos = 0;
+  while ( (pos = source.find(search, pos)) != std::string::npos) {
+  source.replace(pos, search.length(), replace);
+  pos += replace.size();
+  }
+  return source;
 }
 
 void generateTOE(std::ofstream& output_file, const Sleepi::DOXContainer& entries, const std::string_view& title = "") {
-    if (!title.empty())
-        output_file << "# " << title << "\n\n";
+  if (!title.empty())
+  output_file << "# " << title << "\n\n";
 
-    output_file << "## Table of contents : \n";
-    size_t index = 1;
-    for (const auto& [name, entry] : entries) {
-        for (const auto& single_entry : entry) {
-            std::string functionDefinition = single_entry.at(Sleepi::ENTRY_FUNCTION_DEFINTION);
-            functionDefinition.pop_back();
+  output_file << "## Table of contents : \n";
+  size_t index = 1;
+  for (const auto& [name, entry] : entries) {
+  for (const auto& single_entry : entry) {
+      std::string functionDefinition = single_entry.at(Sleepi::ENTRY_FUNCTION_DEFINTION);
+      functionDefinition.pop_back();
 
-            // Markdown really doesn't like these in a header
-            replaceString(functionDefinition, "<", "\\<");
-            replaceString(functionDefinition, ">", "\\>");
+      // Markdown really doesn't like these in a header
+      replaceString(functionDefinition, "<", "\\<");
+      replaceString(functionDefinition, ">", "\\>");
 
-            // Making headers ID'd from 1 to n
-            output_file << "- [" << functionDefinition
-                << "](#" << index++ << ")\n";
-        }
-    } output_file << "- - -\n";
+      // Making headers ID'd from 1 to n
+      output_file << "- [" << functionDefinition
+        << "](#" << index++ << ")\n";
+    }
+  } output_file << "- - -\n";
+}
+
+void generateDocFile(std::ofstream& output_file, const Sleepi::DOXContainer& entries, const std::string_view& title, const std::string_view& source_name){
+
+  using namespace Sleepi;
+
+  // Markdown really likes double newlines
+  constexpr char MD_NL[] = "\n\n";
+  constexpr char H1[] = "# ";
+  constexpr char H2[] = "## ";
+  constexpr char H3[] = "### ";
+
+
+  generateTOE(output_file, entries, title);
+
+  size_t index = 1;
+  for (const auto& [name, entry] : entries) {
+
+    for (const auto& entry_details : entry) {
+
+      std::string functionDefinition = entry_details.at(ENTRY_FUNCTION_DEFINTION);
+      functionDefinition.pop_back();
+
+      output_file << "<h3 id=\"" << index++ << "\"> " << functionDefinition << "</h3>" << MD_NL;
+      output_file << "`" << source_name << "`" << MD_NL;
+      output_file << H3 << "Description:" << MD_NL << entry_details.at(ENTRY_COMMENT) << MD_NL;
+      
+      if (!entry_details.at(ENTRY_PARAMS).empty()) {
+        output_file << H3 << "Params:" << MD_NL;
+        output_file << entry_details.at(ENTRY_PARAMS) << MD_NL;
+      }
+
+      if (!entry_details.at(ENTRY_RETURNS).empty()) {
+        output_file << H3 << "Returns:" << MD_NL;
+        output_file << entry_details.at(ENTRY_RETURNS) << MD_NL;
+      }
+
+      output_file << MD_NL;
+    }
+    output_file << "- - -" << MD_NL;
+  }
+
+
+  output_file << MD_NL << "<p style=\"font-size : 12;\">Made using <a href=\"https://github.com/SleepiCaffeine/sleepiDOX\">sleepiDOX</a></p>";
 }
 
 
+void documentFile(const std::string& directory, std::string destination) {
+  std::ifstream rfile = openReadFile(directory);
+  Sleepi::DOXContainer entries;
+  isolateEntries(extractFileContent(rfile), entries);
 
-void generateDocFile(std::ofstream& output_file, const Sleepi::DOXContainer& entries, const std::string_view& title, const std::string_view& source_name) {
-
-    using namespace Sleepi;
-
-    // Markdown really likes double newlines
-    constexpr char MD_NL[] = "\n\n";
-    constexpr char H1[] = "# ";
-    constexpr char H2[] = "## ";
-    constexpr char H3[] = "### ";
-
-
-    generateTOE(output_file, entries, title);
-
-    size_t index = 1;
-    for (const auto& [name, entry] : entries) {
-
-        for (const auto& entry_details : entry) {
-
-            std::string functionDefinition = entry_details.at(ENTRY_FUNCTION_DEFINTION);
-            functionDefinition.pop_back();
-
-            output_file << "<h3 id=\"" << index++ << "\"> " << functionDefinition << "</h3>" << MD_NL;
-            output_file << "`" << source_name << "`" << MD_NL;
-            output_file << H3 << "Description:" << MD_NL << entry_details.at(ENTRY_COMMENT) << MD_NL;
-            
-            if (!entry_details.at(ENTRY_PARAMS).empty()) {
-                output_file << H3 << "Params:" << MD_NL;
-                output_file << entry_details.at(ENTRY_PARAMS) << MD_NL;
-            }
-
-            if (!entry_details.at(ENTRY_RETURNS).empty()) {
-                output_file << H3 << "Returns:" << MD_NL;
-                output_file << entry_details.at(ENTRY_RETURNS) << MD_NL;
-            }
-
-            output_file << MD_NL;
-        }
-        output_file << "- - -" << MD_NL;
-    }
-
-
-    output_file << MD_NL << "<p style=\"font-size : 12;\">Made using <a href=\"https://github.com/SleepiCaffeine/sleepiDOX\">sleepiDOX</a></p>";
+  // Make output in the same destination as input, just with the .md extension
+  if (destination.empty()) {
+    destination = directory;
+    destination.replace(destination.find_last_of('.'), std::string::npos, ".md");
+  }
+  std::ofstream outputFile = openWriteFile(destination);
+  generateDocFile(outputFile, entries);
 }
--- sleepiDOX.hpp
+++ sleepiDOX.hpp
@@ -6,52 +6,63 @@
 #include <unordered_map>
 
 namespace Sleepi {
-	constexpr size_t ENTRY_FUNCTION_NAME = 0;
-	constexpr size_t ENTRY_COMMENT = 1;
-	constexpr size_t ENTRY_RETURNS = 2;
-	constexpr size_t ENTRY_PARAMS = 3;
-	constexpr size_t ENTRY_FUNCTION_DEFINTION = 4;
-	constexpr size_t ENTRY_FUNCTION_CLASS = 5;
+  constexpr size_t ENTRY_FUNCTION_NAME = 0;
+  constexpr size_t ENTRY_COMMENT = 1;
+  constexpr size_t ENTRY_RETURNS = 2;
+  constexpr size_t ENTRY_PARAMS = 3;
+  constexpr size_t ENTRY_FUNCTION_DEFINTION = 4;
+  constexpr size_t ENTRY_FUNCTION_CLASS = 5;
 
-	// Error bits used in `extractArguments`
-	namespace ErrorBits {
-		constexpr size_t InputDirDoesntExist = 0x01;
-		constexpr size_t NoInputSpecified = 0x02;
-		constexpr size_t NoInputFilesFound = 0x04;
-		constexpr size_t NoOutputFileSpecified = 0x08;
-	};
-	
+  // Error bits used in `extractArguments`
+  namespace ErrorBits {
+  constexpr size_t InputDirDoesntExist = 0x01;
+  constexpr size_t NoInputSpecified = 0x02;
+  constexpr size_t NoInputFilesFound = 0x04;
+  constexpr size_t NoOutputFileSpecified = 0x08;
+  };
+  
 
-	using DOXEntry = std::array<std::string, 5>;
-	using DOXContainer = std::unordered_map< std::string, std::vector <DOXEntry> >;
-	struct DOXContext {
-		std::vector<std::string> sourceDirs;
-		std::string outputFileDir;
-		size_t errorFlags;
-	};
+  using DOXEntry = std::array<std::string, 5>;
+  using DOXContainer = std::unordered_map< std::string, std::vector <DOXEntry> >;
+  struct DOXContext {
+  std::vector<std::string> sourceDirs;
+  std::string outputFileDir;
+  size_t errorFlags;
+  };
 };
 
-/* @sleepiDOX Extracts the command-line arguments passed in. The flags are:
-- -d  : output file destrination/directory.
-- -s  : a single source file that will be parsed.
-- -fs : directory (relative to the executable) that contains header files to be parsed. Header files include *.hpp and *.h files.
+/*
+@sleepiDOX Extracts the command-line arguments passed in. The flags are:
+- d  : output file destrination/directory.
+- s  : a single source file that will be parsed.
+- fs : directory (relative to the executable) that contains header files to be parsed. Header files include *.hpp and *.h files.
+#### Parameters:
+- argv   : vector of std::string arguments passed through the command line
+- rfile  : string that will contain input file.
+- wfile  : string that will contain output file.
+- strict : boolean that determines whether this function will throw exceptions. Set this to false if you want to handle errors in `Sleepi::DOXContext::errorFlags` differently.  #### Returns:
+a `Sleepi::DOXContext` that contains parsed input/output file names.
 */
-// @sleepiPARAM argv   : vector of std::string arguments passed through the command line
-// @sleepiPARAM rfile  : string that will contain input file.
-// @sleepiPARAM wfile  : string that will contain output file.
-// @sleepiPARAM strict : boolean that determines whether this function will throw exceptions. Set this to false if you want to handle errors in `Sleepi::DOXContext::errorFlags` differently.
-// @sleepiRETURNS Returns a `Sleepi::DOXContext` that contains parsed input/output file names.
 Sleepi::DOXContext extractArguments(const std::vector<std::string>& argv, const bool strict = true);
 
 
-/* @sleepiDOX Checks `errorFlags` for every error flag in `Sleepi::ErrorBits` and throws exceptions accordingly.
-Typically called internally, but can be done externally if `extractArguments` doesn't do it on it's own.*/
-// @sleepiPARAM `const Sleepi::DOXContext& context` : DOXContext that will be checked for any error flags set
+/*
+@sleepiDOX Checks `errorFlags` for every error flag in `Sleepi::ErrorBits` and throws exceptions accordingly.
+Typically called internally, but can be done externally if `extractArguments`'s `strict` is set to false.
+#### Parameters:
+- `const Sleepi::DOXContext& context` : DOXContext that will be checked for any error flags set
+*/
 void validateContext(const Sleepi::DOXContext& context);
 
-// @sleepiDOX Fills the provided file output stream with preformatted documentation text.
-// @sleepiPARAM outputFile : output file stream where this function will write to. Does not perform any validation.
-// @sleepiPARAM entries: hashmap of entries to document.
-// @sleepiPARAM title *(optional)*: Text at the top of the page, at header level 1
-// @sleepiPARAM source_name *(optional)*: filename (or any comment) that will be added next to function descriptions to show which file they come from.
+/*
+@sleepiDOX Fills the provided file output stream with preformatted documentation text.
+#### Parameters:
+- outputFile : output file stream where this function will write to. Does not perform any validation.
+- entries: hashmap of entries to document.
+- title *(optional)*: Text at the top of the page, at header level 1
+- source_name *(optional)*: filename (or any comment) that will be added next to function descriptions to show which file they come from.
+*/
 void generateDocFile(std::ofstream& outputFile, const Sleepi::DOXContainer& entries, const std::string_view& title = "", const std::string_view& source_name = "");
+
+
+void documentFile(const std::string& directory, std::string destination = "");
